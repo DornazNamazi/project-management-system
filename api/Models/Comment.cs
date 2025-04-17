e7ff38affe5b4cee6fced7e4c36b6536f3e960ea
DornazNamazi <dornaz82n@gmail.com>
2025-04-16 20:20:59 -0400

    Create-Comment-Controller#9

---
+++ api/Models/Comment.cs
@@ -0,0 +1,14 @@
+namespace API.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; } = default!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public string UserId { get; set; } = default!;
+        public int TaskId { get; set; }
+
+        public TaskItem Title { get; set; } = default!;
+    }
+}
